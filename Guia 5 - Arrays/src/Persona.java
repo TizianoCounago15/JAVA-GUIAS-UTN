3b0fb79617689459c955e7e2420de11f34dc6c83
TizianoCounago15 <counagotiziano@gmail.com>
2025-11-20 18:02:44 -0300

    Agrego carpeta Guia 5 - Arrays

---
+++ Guia 5 - Arrays/src/Persona.java
@@ -0,0 +1,48 @@
+public class Persona {
+    /*4. Crea un programa que declare un array estático de objetos de tipo "Persona"
+(una clase que has creado previamente con propiedades como nombre, edad,
+etc.). Luego, inicializa el array con algunas instancias de "Persona". Luego, utiliza
+un bucle for para imprimir los detalles de cada persona en el array.*/
+    private String nombre;
+    private String apellido;
+    private String dni;
+
+    public Persona(String nombre, String apellido, String dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", dni='" + dni + '\'' +
+                '}';
+    }
+}
